b9b8ac0254ed4e56e1c8a7c1ec370c935920b995
BHUWON12 <bhandaribh12@gamil.com>
2023-07-02 22:28:40 +0530

    2 july

---
+++ java learning/literals.java
@@ -0,0 +1,15 @@
+
+public class literals {
+        public static void main(String args[])
+        {
+        byte a= 101;//byte can support only lessthan 127
+        float f1 = 3.4f;//f is most important to write in float values
+        long l1= 123891208183l;// l is impotant in long values
+        char b='B';
+        System.out.println(f1);
+        System.out.println(l1);
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
